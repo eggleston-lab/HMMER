b65479309fcb842d594f85875b9875cc3932a0e6
Matthew Brockley <67327540+mbrockley1901@users.noreply.github.com>
2020-07-10 17:34:52 -0400

    hmmsearch direct output to HMMER repo

--- McyA_DNA_test.sh
+++ McyA_DNA_test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# slurm template for serial jobs
+# mcyA slurm HMMER
 
 # Set SLURM options
-#SBATCH --job-name=McyADNA_query_test                  # Job name
-#SBATCH --output=McyADNA_query_test-%j.out             # Standard output and error log
+#SBATCH --job-name=McyADNA_query_test2                  # Job name
+#SBATCH --output=/storage/eggleston-research/HMMER/mcyA-output/McyADNA_query_test-%j.out             # Standard output and error log
 #SBATCH --mail-user=mbrockley@middlebury.edu     # Where to send mail	
 #SBATCH --mail-type=ALL                        # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH -N1 -n20 --mem-per-cpu=200M -t01:00:00 --qos=short                         # Time limit hrs:min:sec
@@ -15,7 +15,7 @@ echo "Starting: "`date +"%D %T"`
 
 # Your calculations here
 
-nhmmer mcyADNA.hmm /storage/eggleston-research/metagenomes/lakeerie2011.faa > mcyADNA.hmmout
+hmmsearch /home/mbrockley/mcyADNA.hmm /storage/eggleston-research/metagenomes/lakeerie2011.faa > /storage/eggleston-research/HMMER/mcyA-output/mcyADNA-%j.hmmout
 
 
 # End of job info
